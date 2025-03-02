433170d3e86653dd9795aa5d92117158a111b191
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2025-03-01 20:28:48 -0800

    Update CSVBusSystem.h

--- include/CSVBusSystem.h
+++ include/CSVBusSystem.h
@@ -7,6 +7,8 @@
 class CCSVBusSystem : public CBusSystem{
     private:
         struct SImplementation;
+        struct SStop;
+        struct SRoute;
         std::unique_ptr< SImplementation > DImplementation;
     public:
         CCSVBusSystem(std::shared_ptr< CDSVReader > stopsrc, std::shared_ptr< CDSVReader > routesrc);
@@ -14,10 +16,11 @@ class CCSVBusSystem : public CBusSystem{
 
         std::size_t StopCount() const noexcept override;
         std::size_t RouteCount() const noexcept override;
-        std::shared_ptr<SStop> StopByIndex(std::size_t index) const noexcept override;
-        std::shared_ptr<SStop> StopByID(TStopID id) const noexcept override;
-        std::shared_ptr<SRoute> RouteByIndex(std::size_t index) const noexcept override;
-        std::shared_ptr<SRoute> RouteByName(const std::string &name) const noexcept override;
+        std::shared_ptr<CBusSystem::SStop> StopByIndex(std::size_t index) const noexcept override;
+        std::shared_ptr<CBusSystem::SStop> StopByID(TStopID id) const noexcept override;
+        std::shared_ptr<CBusSystem::SRoute> RouteByIndex(std::size_t index) const noexcept override;
+        std::shared_ptr<CBusSystem::SRoute> RouteByName(const std::string &name) const noexcept override;
+
 };
 
 #endif
